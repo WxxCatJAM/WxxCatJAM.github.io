26afbfe244f92d520fb423e0edf8e81aeb704761
wxxxxxxxxxxxxxxx <460939424@qq.com>
2024-11-11 15:40:36 +0800

    office update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'wx的数学公式总结',
+    description: '主要为数学1的公式',
+    repo: "https://github.com/wxxxxxxxxxxxxxxx/VuePress-MathFunction.git",
+    themeConfig: {
+        // 添加导航栏
+        nav: [
+            { text: 'vue', link: '/' }
+    }
+  }
+  
