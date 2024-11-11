b7d3292a908bd5b49fd99af31932b0f2accf0beb
wxxxxxxxxxxxxxxx <460939424@qq.com>
2024-11-11 16:35:29 +0800

    office update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,27 @@ module.exports = {
     title: 'wx的数学公式总结',
     description: '主要为数学1的公式',
     repo: "https://github.com/wxxxxxxxxxxxxxxx/VuePress-MathFunction.git",
-    themeConfig: {
-        // 添加导航栏
-        nav: [
-            { text: 'vue', link: '/' },
-            { text: 'wx', link: '/wx/' }
-        ]
-    }
+    // themeConfig: {
+    //     // 添加导航栏
+    //     nav: [
+    //         { text: 'vue', link: '/' },
+    //         { text: 'wx', link: '/wx/' }
+    //     ]
+    // }
+    themeConfig:{
+        nav: [{text: "主页", link: "/"      },
+            { text: "node", link: "/node/" },
+            { text: "前端", 
+              items: [ 
+                { text: "html", link:"/web/html/"},
+                { text: "css", link:"/web/css/"},
+                ]
+            },
+            { text: "数据库", link: "/database/"   },
+            { text: "android", link: "/android/"   },
+            { text: "面试问题", link: "/interview/" }
+          ],
+        sidebar: auto
+      }
   }
   
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,14 @@
-'# Hello VuePress' 
+---
+home: true
+heroText: Hero 标题
+features:
+- title: 常用公式
+  details: 如：对数、指数运算、三角函数等基础公式
+- title: 高数
+  details: 如：函数、微分、积分、导数、积分变换等
+- title: 线代
+  details: 如：向量、矩阵、线性方程组、线性变换等
+- title: 概率论与数理统计
+  details: 如：大数定理、贝叶斯定理、概率论等
+footer: MIT Licensed | Copyright © 2024-present wx
+---
