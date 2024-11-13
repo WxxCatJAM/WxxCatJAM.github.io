4fbb163be58c1577089a2d03a7bf4fd3b534e1f5
wxxxxxxxxxxxxxxx <wxcool007@vip.qq.com>
2024-11-13 23:53:42 +0800

    home update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
     title: 'wx的数学公式总结',
     description: '主要为数学1的公式',
+    // host: "127.0.0.1",
+    port: "4001",
     repo: "https://github.com/wxxxxxxxxxxxxxxx/VuePress-MathFunction.git",
     // themeConfig: {
     //     // 添加导航栏
@@ -9,13 +11,25 @@ module.exports = {
     //         { text: 'wx', link: '/wx/' }
     //     ]
     // }
+    plugins: [
+        [
+            'vuepress-plugin-mathjax',
+            {
+                target: 'katex',
+                macros: {
+                "\\RR": "\\mathbb{R}"
+                }
+            }
+        ]
+    ],
     themeConfig:{
-        nav: [{text: "主页", link: "/"},
-            { text: "目录",
+        nav: [
+            {text: "主页", link: "/"},
+            { text: "公式总结",
                 items: [
                     { text: "常用公式", link: "pages/General/General_Formula.md" },
                     { text: "高等数学基础", link: "pages/Advanced/Advanced_Mathematics.md" },
-                    { text: "线性代数", 
+                    { text: "线性代数",  link: "pages/Linear/Matrix.md" 
                     //   items: [ 
                     //     { text: "行列式", link:"./Linear/"},
                     //     { text: "矩阵", link:"./Linear/Matrix.md"},
@@ -28,46 +42,13 @@ module.exports = {
                 ]
             }, 
           ],
-        sidebar: {
-            '/pages/General/':[         
-                {
-                    title: '常用公式',   // 一级菜单名称
-                    collapsable: false, // false为默认展开菜单, 默认值true是折叠,
-                    sidebarDepth: 1,    //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
-                    children: [
-                        ['Advanced_Mathematics.md', '常用公式总结'],  //菜单名称为'子菜单1'，跳转至/pages/folder1/test1.md
-                    ]
-                }
-            ],
-            '/pages/Advanced/':[
-                {
-                    title: '高数公式总结',
-                    collapsable: false, 
-                    children: [
-                        ['Advanced_Mathematics.md.md', '高数公式']
-                    ]
-                }
-            ],
-            '/pages/Linear/':[
-                {
-                    title: '线代公式总结',
-                    collapsable: false, 
-                    children: [
-                        ['Matrix.md', '矩阵']
-                        ['Vector.md', '向量']
-                    ]
-                },
-            ],
-            '/pages/PS/':[
-                {
-                    title: '概率论与数理统计',
-                    collapsable: false, 
-                    children: [
-                        ['PS.md', '概数总结']
-                    ]
-                }
-            ],
-        }
-      }
+        sidebar: [
+            '/',
+            '/pages/General/General_Formula.md',
+            '/pages/Advanced/Advanced_Mathematics.md',
+            '/pages/Linear/Matrix.md',
+            '/pages/PS/PS.md'
+        ],
+        sidebarDepth:2, //使H3也能在侧边栏显示出来
   }
-  
+}
--- docs/pages/General/General_Formula.md
+++ docs/pages/General/General_Formula.md
@@ -1,22 +1,28 @@
-## 预备知识
+# 预备知识
 
  
 此章针对一些基础运算所撰写（如：对数，指数运算等）
 
 ---- 
 
-1. 指数运算
-    a,b 为常数， a,b > 0 且 a,b $$\neq$$1时，有
-    1. $$a^0 = 1$$;
-    2. \((\frac{a}{b})^k = \frac{a^k}{b^k} \)
-    3. $\quad \sqrt[n]{a^k}$
-    4. $a^ka^l=a^{k+l}$
-    5. $(ab)^k=a^k \cdot b^k$
-    6. $(a^k)^l=a^{kl}$
-    7. $\frac{a^k}{a^l} = a^{k-l}$
-2. 对数运算
-    a为常数， a > 0 且 a $\neq$ 1时，有
-    1. $\log_a \frac{1}{M}=-\log_aM$
-    2. $\log_a(MN)=\log_aM+\log_aN$
-    3. $\log_a(M_1M_2\dotsb M_n )= \log_a M_1 + \log_a M_2 + \dots + \log_a M_n $
-    4. $\log_a \frac{M}{N} = \log_a M - \log_aN$
+## 指数运算
+a,b 为常数， a,b > 0 且 a,b $\neq$ 1时，有: 
+1. $a^0 = 1$;
+ 2. $(\frac{a}{b})^k = (\frac{a^k}{b^k})$
+ 3. $\quad \sqrt[n]{a^k}$
+ 4. $a^ka^l=a^{k+l}$
+ 5. $(ab)^k=a^k \cdot b^k$
+ 6. $(a^k)^l=a^{kl}$
+ 7. $\frac{a^k}{a^l} = a^{k-l}$
+
+## 对数运算
+a为常数，$a > 0$ 且 a $\neq$ 1时，有:
+ 1. $\log_a \frac{1}{M}=-\log_aM$
+ 2. $\log_a(MN)=\log_aM+\log_aN$
+ 3. $\log_a(M_1M_2\dotsb M_n )= \log_a M_1 + \log_a M_2 + \dots + \log_a M_n$
+ 4. $\log_a \frac{M}{N} = \log_a M - \log_aN$
+
+## 123
+
+
+## 12312321
